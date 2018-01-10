e200f93bfade6793b876a1de2add721995172799
dylanP3495 <palafoxdylan@gmail.com>
2018-01-10 14:11:24 -0800

    g

--- MinesweeperBoard2.java
+++ MinesweeperBoard2.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 
 public class MinesweeperBoard2{
     Cell[][] board;
-
+    int rows;
+    int columns;
 
     public MinesweeperBoard2(int rows, int columns, int bombs){
         //Put the constructor here.
@@ -26,80 +27,80 @@ public class MinesweeperBoard2{
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        
-        addBombs (bombs);
-        addNums();
+
+        //addBombs (bombs);
+        //addNums();
     }
 
     public MinesweeperBoard2(){
         this (10,10,10);
     }
 
+    /**
     public void addBombs(int bombs){// throws Exception{
-        for (int i = 0; i < bombs; i++) {
+    for (int i = 0; i < bombs; i++) {
 
-            if(board.getValue() == -1 ){
-                i--;
-            }else{
-                board.setValue(-1);
-            }
-        }
+    if(board.getValue() == -1 ){
+    i--;
+    }else{
+    board.setValue(-1);
+    }
+    }
     }
 
     public void addNums(){
-        for (rows = 0; rows < board.length; rows++) {
-            for(columns = 0; columns < board[0].length; columns++){
-                    twoDimensionalArray[row][column] = num;
-                    num ++;
-            }
-            if (board.getValue() == -1 ){//bomb
-                //top left
-                if ((a >= columns) && (a % columns != 0) ){
-                    board[a - columns -1].addValue();
-                }
-
-                // top bound
-                if (a >= columns){
-                    board[a - columns].addValue();
-                }
-
-                // top right
-                if ((a >= columns) && (a % columns != (columns - 1))){
-                    board[a - columns + 1].addValue();
-                }
-
-                // left bound
-                if ((a % columns != 0)){
-                    board[a - 1].addValue();
-                }
-                
-                // right bound
-                if (a % columns != (columns - 1)){
-                    board[a + 1].addValue();
-                }
-                
-                // bot left
-                if ((a <= ((rows-1) * columns)) && (a % columns != 0)){
-                    board[a + columns - 1].addValue();
-                }
-                
-                // bot bound
-                if (a < ((rows-1) * columns)){
-                    board[a + columns].addValue();
-                }
-                
-                // bot right
-                if (a < ((rows - 1) * columns) && (a % columns !=(columns - 1))){
-                    board[a + columns + 1].addValue();
-                }
-            }
-        }
+    for (rows = 0; rows < board.length; rows++) {
+    for(columns = 0; columns < board[0].length; columns++){
+    twoDimensionalArray[row][column] = num;
+    num ++;
+    }
+    if (board.getValue() == -1 ){//bomb
+    //top left
+    if ((a >= columns) && (a % columns != 0) ){
+    board[a - columns -1].addValue();
     }
 
-    /**
-     * This method is used for testing and will be deleted if using the GUI.
-     *  It is still required for all students.
-     **/
+    // top bound
+    if (a >= columns){
+    board[a - columns].addValue();
+    }
+
+    // top right
+    if ((a >= columns) && (a % columns != (columns - 1))){
+    board[a - columns + 1].addValue();
+    }
+
+    // left bound
+    if ((a % columns != 0)){
+    board[a - 1].addValue();
+    }
+
+    // right bound
+    if (a % columns != (columns - 1)){
+    board[a + 1].addValue();
+    }
+
+    // bot left
+    if ((a <= ((rows-1) * columns)) && (a % columns != 0)){
+    board[a + columns - 1].addValue();
+    }
+
+    // bot bound
+    if (a < ((rows-1) * columns)){
+    board[a + columns].addValue();
+    }
+
+    // bot right
+    if (a < ((rows - 1) * columns) && (a % columns !=(columns - 1))){
+    board[a + columns + 1].addValue();
+    }
+    }
+    }
+    }
+
+    //This method is used for testing and will be deleted if using the GUI.
+    //It is still required for all students.
+     */
     public void printBoard(){
         int x = 0;
         for (int i = 0; i < rows; i++){
